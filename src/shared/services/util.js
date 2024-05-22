df1c6f6c16558d71e2cb6a818bde7d46854a7265
Oleg Kucherenko <oleg.kucherenko.developer@gmail.com>
2024-05-22 20:38:20 +0300

    List default workouts

--- src/shared/services/util.js
+++ src/shared/services/util.js
@@ -30,3 +30,25 @@ export const buildAlertsList = (obj, messageType) => {
     console.log(alertsList);
     return alertsList;
 }
+
+export const truncateStringWithWordBoundary = (inputString, maxLength) => {
+    if (inputString && inputString.length > maxLength) {
+        const truncatedText = inputString.substring(0, maxLength);
+        const lastSpaceIndex = truncatedText.lastIndexOf(' ');
+        return lastSpaceIndex !== -1 ? truncatedText.substring(0, lastSpaceIndex) + '...' : truncatedText + '...';
+    }
+    return inputString;
+}
+
+export const getStringOrNull = (value) => {
+    if (value === null || value.trim() === "") {
+        return null;
+    }
+    return value.trim();
+}
+
+export const upperCaseStringToRegular = (string = "ABC") => {
+    let formatted = string[0];
+    formatted += (string.substring(1, string.length)).toLowerCase();
+    return formatted;
+}
--- src/workouts/components/WorkoutComponent.js
+++ src/workouts/components/WorkoutComponent.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardSubtitle from 'react-bootstrap/CardSubtitle';
+import { truncateStringWithWordBoundary } from '../../shared/services/util';
+import { CardText } from 'react-bootstrap';
 
-function Workout({ id, title, description, isCustom }) {
+function Workout({ id, title, description, isCustom, needsEquipment, bodyParts }) {
     return (
-        <div>
-            <h2>{title}</h2>
-            <p>ID: {id}</p>
-            <p>Description: {description}</p>
-            {isCustom && <p>This is a custom workout.</p>}
-        </div>
+        <Card style={{ width: '18rem', minWidth: '18rem', padding: 6, margin: 6, borderWidth: 1, boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.3)' }}>
+            {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+            <Card.Body>
+                <Card.Title className='work-sans-font'>{truncateStringWithWordBoundary(title, 25)}</Card.Title>
+                <CardSubtitle style={{ marginBottom: 10 }}>{isCustom ? 'Custom' : 'In-App'}, {needsEquipment ? 'With equipment' : 'Without equipment'}</CardSubtitle>
+                <Card.Text style={{ height: 120 }} className='merriweather-sans-font'>{truncateStringWithWordBoundary(description, 120)}</Card.Text>
+                <CardText style={{ height: 60 }} >
+                    {bodyParts.map(bodyPart => (
+                        <span className="body-part-tags" key={bodyPart.id}>
+                            <small className="body-part-tag">{bodyPart.name.toLowerCase()}</small>&nbsp;
+                        </span>
+                    ))}
+                </CardText>
+                <Link to={`/workouts/default/${id}`}>
+                    <Button variant="primary">Detail</Button>
+                </Link>
+            </Card.Body>
+        </Card>
     );
 }
 
